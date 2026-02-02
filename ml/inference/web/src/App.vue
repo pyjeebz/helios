31d578081980c8cbc27078c2f4c6b738bb57490f
pyjeebz <lawalsakamujeeb@gmail.com>
2026-02-02 10:51:45 -0500

    feat(ui): add Helios logo and core Vue files

---
+++ ml/inference/web/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import { useThemeStore } from '@/stores/theme'
+import { onMounted } from 'vue'
+
+const themeStore = useThemeStore()
+
+onMounted(() => {
+  themeStore.init()
+})
+</script>
+
+<template>
+  <RouterView />
+</template>
